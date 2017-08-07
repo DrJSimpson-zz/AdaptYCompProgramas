ac28fe32b211a17b172e5d5f599017c3f2f23a62
Guido Benedetti <gbenedetti.1990@gmail.com>
2017-08-07 14:53:32 -0300

    small changes

--- Arreglos.java
+++ Arreglos.java
@@ -5,11 +5,17 @@ public class Arreglos {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int tamanio = sc.nextInt();
+		//
 		int[] ejemplo2 = new int[tamanio];
+		//itero para cargar
+		//nombreDelArreglo.length me retorna el tamaño del arreglo
 		for (int i = 0; i < ejemplo2.length; i++) {
 			ejemplo2[i] = sc.nextInt();
 		}
 
+		//por lógica, lo mas sencillo es iterar desde 0
+		//hasta 1 antes del tamaño del arreglo
+		//itero para mostrar
 		System.out.println("-------------------------");
 
 		for (int i = 0; i < ejemplo2.length; i++) {
@@ -18,16 +24,22 @@ public class Arreglos {
 	}
 
 	public static void arreglosEj1() {
+		//es imposible enumerar variables
+		//y acceder de forma indexada
 		//int a, b, c, d, e;
 		Scanner s = new Scanner(System.in);
 
+		//la solución son los arreglos
 		int[] array = new int[5];
+		//tipo de dato y [] dimensiones
+		//accediendo a los índices con un literal
 		array[0] = 10;
 		array[1] = 45;
 		array[2] = s.nextInt();
 		array[3] = 78;
 		array[4] = s.nextInt();
 
+		//accedo utilizando valores literales
 		System.out.println(array[0]);
 		System.out.println(array[1]);
 		System.out.println(array[2]);
